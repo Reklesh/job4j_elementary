87ad765f6a6897dc5749d66504f14fc2e272580d
Acer <a-krickiy@mail.ru>
2023-04-13 15:10:29 +0300

    7. Расстояние между точками в системе координат [#188]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -10,6 +10,6 @@ public class Point {
         double result = Point.distance(0, 0, 2, 0);
         double result2 = Point.distance(106, 3, 78, 37);
         System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result2 (106, 3) to (78, 37) " + result2);
+        System.out.println("result2 (107, 3) to (78, 37) " + result2);
     }
 }
